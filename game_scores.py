31a4e6ecae9da9e46497207ee48424b058afdddd
John Galbraith <jagalbraith@icloud.com>
2017-04-10 22:51:37 -0500

    Used mlbgame library

--- game_scores.py
+++ game_scores.py
@@ -1,39 +1,11 @@
 #!/usr/bin/env python
 
-import subprocess
+import mlbgame
+import datetime
 
-def scores():
-    command = 'lynx -nonumbers -dump http://www.baseball-reference.com/boxes/ | grep Final -A 1 | grep -A 1 -B 1 Cubs | xargs'
-    bark = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
-    moo = bark.communicate()
-    dict = {}
-    moo = moo[0].replace('-- ', '').replace('\n', '').split(' ')
-    if 'Final' in moo:
-        #print moo
-        dict['status'] = 'Final'
-        moo.pop(moo.index('Final'))
-    else:
-        dict['status'] = 'In Progress'
+year, month, day = str(datetime.datetime.now().strftime('%Y,%-m,%-d')).split(',')
 
-    tb = []
-    for item in moo:
-        if item is not 'Final' and item.isalpha():
-           tb.append(item)
-        elif item.isdigit():
-            dict['{city} {name}'.format(city=tb[0], name=tb[1])] = int(item)
-            tb = []
-    return dict
+scores = mlbgame.day(int(year), int(month), int(day), home="Cubs")
 
-
-def main():
-    scored = scores()
-    for key in scored.keys():
-        if key is 'status':
-            print 'Game Status: {}'.format(scored[key])
-        else:
-            print '{team}: {score}'.format(team=key, score=scored[key])
-
-
-if '__main__' in __name__:
-    main()
-
+for score in scores:
+    print score
